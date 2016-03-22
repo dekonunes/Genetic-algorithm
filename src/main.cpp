aec397ffe74cb67ec977539ff9232c406dd21883
André Nunes <dekonunesss@gmail.com>
2016-03-21 22:54:39 -0300

    Implementado tranformação de decimal para binário e vice-versa

--- src/main.cpp
+++ src/main.cpp
@@ -18,12 +18,11 @@ int main() {
 
 	pop.print_populacao();
 	for (i = 0; i < 50000; ++i) {
-	//for (i = 0; pop.getBestIndividuo().getFitness() != 19; ++i) {
+		//for (i = 0; pop.getBestIndividuo().getFitness() != 19; ++i) {
 		cout << "--------- Geração: " << i << endl;
 		pop.mutacaoPopulacao();
 		newpop = pop.crossoverRollet();
 		//newpop.print_populacao();
-
 
 		//cout << pop.getBestIndividuo().getCromossomo() << endl;
 		//pop.print_populacao();
@@ -32,4 +31,24 @@ int main() {
 	}
 
 	return 0;
+}
+
+string DecToBin(int number) {
+	if (number == 0)
+		return "0";
+	if (number == 1)
+		return "1";
+
+	if (number % 2 == 0)
+		return DecToBin(number / 2) + "0";
+	else
+		return DecToBin(number / 2) + "1";
+}
+
+int BinToDec(string number) {
+	int result = 0, pow = 1;
+	for (int i = number.length() - 1; i >= 0; --i, pow <<= 1)
+		result += (number[i] - '0') * pow;
+
+	return result;
 }
